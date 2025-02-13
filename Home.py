627e0f5983e51326dd73eddd396886170c37ebb7
major-final <likki3287@gmail.com>
2025-02-13 17:59:19 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,27 +5,23 @@ st.set_page_config(
     page_icon="🛣️",
 )
 
-
 st.divider()
 st.title("Road Damage Detection Application")
 
-
 st.markdown(
     """
-    Introducing our Road Damage Detection Apps, powered by the YOLOv8 deep learning model trained on Crowdsensing-based Road Damage Detection Challenge 2022 Dataset.
+    Introducing our Road Damage Detection Apps, powered by the YOLOv8 deep learning model trained on the Crowdsensing-based Road Damage Detection Challenge 2022 Dataset.
     
     This application is designed to enhance road safety and infrastructure maintenance by swiftly identifying and categorizing various forms of road damage, such as potholes and cracks.
 
     There are four types of damage that this model can detect:
-    - Longitudinal Crack
-    - Transverse Crack
-    - Alligator Crack
-    - Potholes
+    - **Longitudinal Crack**
+    - **Transverse Crack**
+    - **Alligator Crack**
+    - **Potholes**
 
-    The model is trained on the YOLOv8 small model using Japan and India CRDDC2022 dataset.
-
+    The model is trained on the YOLOv8 small model using the Japan and India CRDDC2022 dataset.
+    """
 )
 
 st.divider()
-
-
