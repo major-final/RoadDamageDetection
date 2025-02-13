f71751ad42a923a1ed3bb0ebcb3c65df91af08e9
major-final <likki3287@gmail.com>
2025-02-13 17:57:52 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -24,22 +24,10 @@ st.markdown(
 
     The model is trained on the YOLOv8 small model using Japan and India CRDDC2022 dataset.
 
-    You can select the app from the sidebar to try and experiment with different inputs **(real-time webcam, video, and images)** depending on your use case.
-
-    #### Documentation and Links
-    - GitHub Project Page [GitHub](https://github.com/oracl4/RoadDamageDetection)
-    - Contact: it.mahdi.yusuf@gmail.com
-
-    #### License and Citations
-    - Road Damage Dataset from Crowdsensing-based Road Damage Detection Challenge (CRDDC2022)
-    - All rights reserved under YOLOv8 license permitted by [Ultralytics](https://github.com/ultralytics/ultralytics) and the [Streamlit](https://streamlit.io/) framework
-"""
 )
 
 st.divider()
 
 st.markdown(
-    """
-    This project was created for the [Road Damage Detection Challenge](https://s.id/RDDHariJalan23) by the [Ministry of Public Works and Housing](https://pu.go.id/) in celebration of "Peringatan Hari Jalan 2023".
-    """
+   
 )
